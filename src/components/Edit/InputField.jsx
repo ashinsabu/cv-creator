b5ee5b5a9230cea01b92a76e6e87f54e6ac7cc40
ashinsabu <ashin.sabu3@gmail.com>
2023-02-08 19:04:27 +0530

    InputField is now a functional component

--- src/components/Edit/InputField.jsx
+++ src/components/Edit/InputField.jsx
@@ -2,20 +2,17 @@ import React, { Component } from 'react'
 import '../../styles/App.css'
 import './InputField.css'
 
-class InputField extends Component {
-    render() { 
+function InputField (props) {
         return ( 
             <input 
             type="text" 
-            placeholder={this.props.placeholder} 
+            placeholder={props.placeholder} 
             className='inputField' 
-            name={this.props.name} 
-            onChange={this.props.onChange} 
-            defaultValue={this.props.value?this.props.value:""}
-            
+            name={props.name} 
+            onChange={props.onChange} 
+            defaultValue={props.value?props.value:""}
             />
          );
-    }
 }
  
 export default InputField;
