0922b4c0ab901ee4f0561a6f727994109ff49a3d
ashinsabu <ashin.sabu3@gmail.com>
2023-02-08 23:21:02 +0530

    Readme update

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # CV Creator
 
 A React app which allows users to create their own resume using a ready template and download it in a PDF or DOC format.
-Now uses functional components and hooks as of 8 Feb 2023
+Now uses functional components and hooks as of 8th February, 2023.
 
-<a href="https://ashinsabu.github.io/cv-creator/" target="_blank">Live Demo</a> :point_left:
+<a href="https://ashinsabu.github.io/cv-creator/" target="_blank" style="font-size: 24px;">Live Demo</a> :point_left:
 
 ## Getting started
 
@@ -19,3 +19,5 @@ npm start
 - [React](https://reactjs.org/)
 - [uniqid](https://www.npmjs.com/package/uniqid)
 - [gh-pages](https://www.npmjs.com/package/gh-pages)
+- [react-to-print](https://github.com/gregnb/react-to-print)
+
---
+++ build/precache-manifest.ae473176191dda3eb0fdfacc39c8a653.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba68fc36f55716d6f97d",
+    "url": "/cv-creator/static/css/main.38c283d9.chunk.css"
+  },
+  {
+    "revision": "ba68fc36f55716d6f97d",
+    "url": "/cv-creator/static/js/main.3d30326f.chunk.js"
+  },
+  {
+    "revision": "e280d705ce81bbee6bea",
+    "url": "/cv-creator/static/js/runtime~main.4dc9ccd2.js"
+  },
+  {
+    "revision": "46f5e9a8b4e676e6f2ea",
+    "url": "/cv-creator/static/js/2.3458fe08.chunk.js"
+  },
+  {
+    "revision": "4af07b562c0602e03473",
+    "url": "/cv-creator/static/js/3.a772fac8.chunk.js"
+  },
+  {
+    "revision": "996eb640641aa4f8568dcef6922ba509",
+    "url": "/cv-creator/static/media/phone-square-solid.996eb640.svg"
+  },
+  {
+    "revision": "46bb59058ba39a53b02a082fd30eb897",
+    "url": "/cv-creator/static/media/envelope-square-solid.46bb5905.svg"
+  },
+  {
+    "revision": "2b7c48a767e22546941d70afc9f27ff0",
+    "url": "/cv-creator/static/media/map-marker-alt-solid.2b7c48a7.svg"
+  },
+  {
+    "revision": "29e16047498195f7b87f97687db6da5c",
+    "url": "/cv-creator/index.html"
+  }
+];
