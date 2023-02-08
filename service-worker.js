86c1b38f69ad983a07aa6889a3506d3e24130bc1
ashinsabu <ashin.sabu3@gmail.com>
2023-02-08 23:18:42 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cv-creator/precache-manifest.a1df1c07ffdc5f01c6a9aa18ed4a900f.js"
+  "/cv-creator/precache-manifest.ae473176191dda3eb0fdfacc39c8a653.js"
 );
 
 workbox.clientsClaim();
